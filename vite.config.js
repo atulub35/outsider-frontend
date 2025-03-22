4571744234115a4414edc7b99df2f1f4aec24efe
Atul Ubarhande <atul612@gmail.com>
2025-03-22 13:57:18 +0530

    added host url to allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
   },
   preview: {
     host: true,
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    allowedHosts: [
+      'outsider-react-8de83560ff04.herokuapp.com',
+      '.herokuapp.com'  // This will allow all herokuapp.com subdomains
+    ]
   }
 })
