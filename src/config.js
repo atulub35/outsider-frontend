9b8f07fd59e251bc7e86431ef3ac293502c66d62
Atul Ubarhande <atul612@gmail.com>
2025-03-29 19:03:40 +0530

    updated host url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === "production"
-  ? "https://outsider-284373f8936c.herokuapp.com/"
+  ? "https://outsider-284373f8936c.herokuapp.com"
   : "http://localhost:3001"
