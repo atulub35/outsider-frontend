b489aac205819621c1a46cd150ae12442a26fc25
Atul Ubarhande <atul612@gmail.com>
2025-03-29 17:47:51 +0530

    updated domain url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === "production"
-  ? "https://outsider-284373f8936c.herokuapp.com"
+  ? "https://outsider-react-8de83560ff04.herokuapp.com"
   : "http://localhost:3001"
